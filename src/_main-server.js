52393dab355108f67cd21f429a51e7e5965aeed7
mankey-ru <mankey-ru@yandex.ru>
2017-03-01 18:10:52 +0300

    port adjustment for Heroku http://stackoverflow.com/a/15693371

--- src/_main-server.js
+++ src/_main-server.js
@@ -4,7 +4,9 @@ var app = express();
 require('./api.js')(app);
 app.use(express.static(__dirname + './../www/'));
 
-var port = 8081;
+var port = process.env.PORT || 8081;
+// process.env.PORT works on Heroku   http://stackoverflow.com/a/15693371
+
 app.listen(port, function () {
 	console.log(' Web server started at port ' + port);
 })
